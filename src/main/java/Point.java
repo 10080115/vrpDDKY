5c38380287eb84923d618baa5fe732eaa317578e
li <li@lideMacBook-Air.local>
2020-03-12 17:06:53 +0800

    初始提交

---
+++ src/main/java/Point.java
@@ -0,0 +1,174 @@
+/**
+ * @author liShiWei
+ * @date 2020-03-12
+ */
+class Point {
+    /**
+     * 站点编号
+     */
+    private Integer stationId;
+
+    /**
+     * 取点纬度
+     */
+    private Double fetchLatitude;
+
+    /**
+     * 取点经度
+     */
+    private Double fetchLongitude;
+
+    /**
+     * 运单编号
+     */
+    private Long orderId;
+
+    /**
+     * 送点纬度
+     */
+    private Double sendLatitude;
+
+    /**
+     * 送点经度
+     */
+    private Double sendLongitude;
+
+    /**
+     * 运单类型
+     */
+    private Integer shippingType;
+
+    /**
+     * 审核时间
+     */
+    private Long auditUnixTime;
+
+    /**
+     * 考核时间
+     */
+    private Long expectArrivalUnixTime;
+
+    /**
+     * 预估到达时间(路径规划给出)
+     */
+    private Long estimateUnixTime;
+
+    /**
+     * 是否为再投运单
+     */
+    private boolean orderHang;
+
+    /**
+     * 再投运单约定配送时间
+     */
+    private Long orderHangSendUnixTime;
+
+    /**
+     * 指派时刻
+     */
+    private Long dispatchUnixTime;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Double getSendLatitude() {
+        return sendLatitude;
+    }
+
+    public void setSendLatitude(Double sendLatitude) {
+        this.sendLatitude = sendLatitude;
+    }
+
+    public Double getSendLongitude() {
+        return sendLongitude;
+    }
+
+    public void setSendLongitude(Double sendLongitude) {
+        this.sendLongitude = sendLongitude;
+    }
+
+    public Integer getShippingType() {
+        return shippingType;
+    }
+
+    public void setShippingType(Integer shippingType) {
+        this.shippingType = shippingType;
+    }
+
+    public Long getAuditUnixTime() {
+        return auditUnixTime;
+    }
+
+    public void setAuditUnixTime(Long auditUnixTime) {
+        this.auditUnixTime = auditUnixTime;
+    }
+
+    public Long getExpectArrivalUnixTime() {
+        return expectArrivalUnixTime;
+    }
+
+    public void setExpectArrivalUnixTime(Long expectArrivalUnixTime) {
+        this.expectArrivalUnixTime = expectArrivalUnixTime;
+    }
+
+    public Long getEstimateUnixTime() {
+        return estimateUnixTime;
+    }
+
+    public void setEstimateUnixTime(Long estimateUnixTime) {
+        this.estimateUnixTime = estimateUnixTime;
+    }
+
+    public Long getOrderHangSendUnixTime() {
+        return orderHangSendUnixTime;
+    }
+
+    public void setOrderHangSendUnixTime(Long orderHangSendUnixTime) {
+        this.orderHangSendUnixTime = orderHangSendUnixTime;
+    }
+
+    public boolean isOrderHang() {
+        return orderHang;
+    }
+
+    public void setOrderHang(boolean orderHang) {
+        this.orderHang = orderHang;
+    }
+
+    public Integer getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(Integer stationId) {
+        this.stationId = stationId;
+    }
+
+    public Double getFetchLatitude() {
+        return fetchLatitude;
+    }
+
+    public void setFetchLatitude(Double fetchLatitude) {
+        this.fetchLatitude = fetchLatitude;
+    }
+
+    public Double getFetchLongitude() {
+        return fetchLongitude;
+    }
+
+    public void setFetchLongitude(Double fetchLongitude) {
+        this.fetchLongitude = fetchLongitude;
+    }
+
+    public Long getDispatchUnixTime() {
+        return dispatchUnixTime;
+    }
+
+    public void setDispatchUnixTime(Long dispatchUnixTime) {
+        this.dispatchUnixTime = dispatchUnixTime;
+    }
+}
---
+++ src/main/java/vrp.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author liShiWei
+ * @date 2020-03-12
+ */
+public class vrp {
+    static String filePath = "/Users/li/Desktop/code/20160219/service/vrp/src/main/resources/jieguo.txt";
+    public static void main(String[] args) {
+        Map<Long, List<Point>> points = getVrpInput(filePath);
+
+        //3.使用for循环利用EntrySet进行遍历
+        for(Map.Entry<Long,List<Point>>entry: points.entrySet()){
+            System.out.println("Item :"+entry.getKey()+" Count:"+entry.getValue().size());
+        }
+    }
+
+
+    static Map<Long, List<Point>> getVrpInput(String filePath){
+        Map<Long, List<Point>> batchId2Points = new HashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath))));
+            String lineTxt = br.readLine();
+            while ((lineTxt = br.readLine()) != null) {
+                String[] rawData = lineTxt.split("\t");
+
+                Point p = new Point();
+                Long batchNo = Long.parseLong(rawData[0]);
+                p.setOrderId(Long.parseLong(rawData[2]));
+                p.setDispatchUnixTime(Long.parseLong(rawData[3]));
+                p.setStationId(Integer.parseInt(rawData[6]));
+                p.setSendLatitude(Double.parseDouble(rawData[7]));
+                p.setSendLongitude(Double.parseDouble(rawData[8]));
+                p.setShippingType(Integer.parseInt(rawData[10]));
+                p.setExpectArrivalUnixTime(Long.parseLong(rawData[14]));
+                p.setAuditUnixTime(Long.parseLong(rawData[13]));
+
+                //暂时先用默认值
+                p.setOrderHang(false);
+                p.setOrderHangSendUnixTime(1L);
+                p.setFetchLatitude(39.924695);
+                p.setFetchLongitude(116.421868);
+                p.setEstimateUnixTime(1L);
+
+                List<Point> points;
+                if(batchId2Points.containsKey(batchNo)){
+                    points = batchId2Points.get(batchNo);
+                }else{
+                    points = new ArrayList<>();
+                }
+
+                points.add(p);
+                batchId2Points.put(batchNo, points);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("read errors :" + e);
+        }
+        return batchId2Points;
+    }
+}
